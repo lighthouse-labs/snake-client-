445e6bb9f81e679d4187cdca0b9e707bd0a94187
zinat <zinat.gholami@gmail.com>
2021-11-03 23:20:43 +0000

    Client setup Assignment

---
+++ clientCode.js
@@ -0,0 +1,22 @@
+// Client <------------
+// Chat app, where you can talk to me not using zoom, or slack 
+// but YOUR OWN TERMINAL
+
+const net = require('net');
+const stdin = process.stdin;
+                                            //127.0.0.1
+const name = 'Vas';
+const client = net.createConnection({host: 'localhost', port: 3001});
+stdin.setEncoding('utf8');
+client.setEncoding('utf8');
+
+client.write(`${name} has connected!!!`);
+
+client.on('data', (data) => {
+    console.log("Message from server recieved!!!");
+    console.log(data);
+})
+
+stdin.on('data', (input) => {
+    client.write(`${name}: ${input}`);
+})

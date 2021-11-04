5b7af019338d22c728bb6d2e526507556e414ad5
zinat <zinat.gholami@gmail.com>
2021-11-04 00:40:26 +0000

    Connection module

--- play.js
+++ play.js
@@ -1,22 +1,10 @@
+// const connect = require("connect");
+
+const {reza, connection} = require("../snek-multiplayer/client");
 const net = require("net");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  })
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on('data', (data) => {  
-  console.log(data);
-})
-
-  return conn;
-};
-
 console.log("Connecting ...");
-connect();
+console.log("Reza is "+ reza().name);
 
 
 
